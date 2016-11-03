4859c5c3b74f0a79e39ca19d1c61df3c84e35fa6
CarloSolis <Cali.1997@aol.com>
2016-11-02 23:23:01 -0600

    Falta aun

--- Podium/NewUserVC.swift
+++ Podium/NewUserVC.swift
@@ -8,22 +8,27 @@
 
 import UIKit
 
-class NewUserVC: UIViewController {
+class NewUserVC: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var StackViewTextField: UIStackView!
-    @IBOutlet weak var tfName: UITextField!
-    @IBOutlet weak var tfLastName: UITextField!
-    @IBOutlet weak var tfEmail: UITextField!
-    @IBOutlet weak var tfPassword: UITextField!
-    @IBOutlet weak var tfVPassword:UITextField!
-    @IBOutlet weak var btnCreate: UIButton!
-    @IBOutlet weak var tfCode: UITextField!
+    @IBOutlet weak var tfName: FieldsUI!
+    @IBOutlet weak var tfLastName: FieldsUI!
+    @IBOutlet weak var tfLastName2: FieldsUI!
+    @IBOutlet weak var tfEmail: FieldsUI!
+    @IBOutlet weak var tfPassword: FieldsUI!
+    @IBOutlet weak var tfVPassword: FieldsUI!
+    @IBOutlet weak var tfPhone: FieldsUI!
+    @IBOutlet weak var tfAddress: FieldsUI!
+    @IBOutlet weak var tfBirthday: FieldsUI!
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var btnDone: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //  self.tfEmail.delegate = self
     }
     
-    @IBAction func BackBtnPressed(_ sender: AnyObject) {
+    @IBAction func BackBtnPressed(sender: AnyObject) {
         
         
         dismiss(animated: true, completion: nil)
@@ -31,12 +36,42 @@ class NewUserVC: UIViewController {
     }
     
     @IBAction func CreateAccountPressed(_ sender: AnyObject) {
-
         
         //METODO
-        dismiss(animated: true, completion: nil)
         
     }
     
+    
+    @IBAction func tfEditing(_ sender: UITextField) {
+        
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        
+        sender.inputView = datePickerView
+        
+        datePickerView.addTarget(self, action: #selector(NewUserVC.datePickerValueChanged), for: UIControlEvents.valueChanged)
+        
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        
+        tfBirthday.text = dateFormatter.string(from: sender.date)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        self.view.endEditing(true)
+        
+        return true
+        
+    }
     
 }
