9dc0dd38673c33c9a2daebec5e12bb639087906c
CarloSolis <Cali@Carloss-MBP.Home>
2016-11-10 15:48:14 -0600

    Fix

--- Podium/ObserverVC.swift
+++ Podium/ObserverVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ObserverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet weak var tableView: UITableView!
     var comment: Comment!
     var comments = [Comment]()
@@ -17,7 +17,7 @@ class ObserverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var lblEtapa: UILabel!
     @IBOutlet weak var lblDate: UILabel!
     @IBOutlet weak var navBar: UINavigationBar!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLbl()
@@ -25,47 +25,51 @@ class ObserverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func BackPressed(_ sender: AnyObject) {
-        
+
         dismiss(animated: true, completion: nil)
-        
+
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return comments.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         if let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell", for: indexPath)as? CommentCell{
             let comment = comments[indexPath.row]
             cell.configureCell(comment: comment)
             return cell
-            
+
         } else {
-            
+
             return CommentCell()
-            
+
         }
     }
-    
+
     func updateLbl (){
-    
-        self.navBar.topItem?.title = debate.name
-       // lblEtapa.text = debate.description
-        lblEtapa.layer.cornerRadius = 8.0
-        lblDate.text = "\(debate.startingDate)"
-        lblDate.layer.cornerRadius = 8.0
+
+        //self.navBar.topItem?.title = debate.name
+        // lblEtapa.text = debate.description
+        //  lblDate.text = "\(debate.startingDate)"
+
+        lblEtapa.layer.masksToBounds = true
+        lblEtapa.layer.cornerRadius = 5
+        lblDate.layer.masksToBounds = true
+        lblDate.layer.cornerRadius = 5
+        
     }
     
     
