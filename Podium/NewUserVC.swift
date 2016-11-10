f370a3fe225d8251a641d000c82bed6b2c9f7963
CarloSolis <Cali@Carloss-MBP.Home>
2016-11-10 12:13:30 -0600

    Delete Picker

--- Podium/NewUserVC.swift
+++ Podium/NewUserVC.swift
@@ -11,7 +11,7 @@ import Alamofire
 import SCLAlertView
 
 class NewUserVC: UIViewController, UITextFieldDelegate {
-    
+
     @IBOutlet weak var StackViewTextField: UIStackView!
     @IBOutlet weak var tfName: FieldsUI!
     @IBOutlet weak var tfLastName: FieldsUI!
@@ -25,78 +25,62 @@ class NewUserVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tfidUniversity: FieldsUI!
     var reach: Reachability!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         reach = Reachability()
-        
-    }
-    
-    @IBAction func BackBtnPressed(sender: AnyObject) {
-        
-        
-        dismiss(animated: true, completion: nil)
-        
-    }
-    
-    @IBAction func CreateAccountPressed(_ sender: AnyObject) {
-        
-        //METODO
-        
-    }
-    
-    
-    @IBAction func tfEditing(_ sender: UITextField) {
-        
-        let datePickerView:UIDatePicker = UIDatePicker()
-        
-        datePickerView.datePickerMode = UIDatePickerMode.date
-        
-        sender.inputView = datePickerView
 
     }
+
+    @IBAction func BackBtnPressed(sender: AnyObject) {
+
+
+        dismiss(animated: true, completion: nil)
+
+    }
+
 
     func keyboardWillShow(notification:NSNotification){
         //give room at the bottom of the scroll view, so it doesn't cover up anything the user needs to tap
         var userInfo = notification.userInfo!
         var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
         keyboardFrame = self.view.convert(keyboardFrame, from: nil)
-        
+
         var contentInset:UIEdgeInsets = self.scrollView.contentInset
         contentInset.bottom = keyboardFrame.size.height
         self.scrollView.contentInset = contentInset
     }
-    
+
     func keyboardWillHide(notification:NSNotification){
         let contentInset:UIEdgeInsets = UIEdgeInsets.zero
         self.scrollView.contentInset = contentInset
     }
-    
-    
+
+
     func createUser(_ completed: @escaping DownloadComplete){
-        
+
         let NEW_USER_URL = "\(BASE_URL)\(REGISTER_URL)\(NAME_URL)\(tfName.text!)\(LASTNAME_URL)\(tfLastName.text!)\(LASTNAME2_URL)\(tfLastName2.text!)\(EMAILN_RL)\(tfEmail.text!)\(PASSWORD_URL)\(tfPassword.text!)\(PHONE_URL)\(tfPhone.text!)\(ADDRESS_URL)\(tfAddress.text!)\(IDUNIVERSITY_URL)\(tfidUniversity.text!)"
-        
+
         Alamofire.request(NEW_USER_URL).responseJSON {response in
             let result = response.result
-            
+
             print(response, result, " -> URL: \(NEW_USER_URL)")
-            
+
             if (result.value as? Dictionary<String, AnyObject>) != nil{
-                
+
                 print("NONE")
-                
+
             }
-            
+
             completed()
         }
     }
-    
+
     @IBAction func createUserPressed(_ sender: Any) {
         if (tfName.text!.isEmpty || tfLastName.text!.isEmpty || tfLastName2.text!.isEmpty || tfEmail.text!.isEmpty || tfPassword.text!.isEmpty || tfVPassword.text!.isEmpty || tfAddress.text!.isEmpty || tfPhone.text!.isEmpty || tfidUniversity.text!.isEmpty) {
-            
+
             SCLAlertView().showError("Campos Requeridos", subTitle: "Todos los campos son requeridos")
-            
+
             tfName.layer.backgroundColor = UIColor.red.cgColor
             tfLastName.layer.backgroundColor = UIColor.red.cgColor
             tfLastName2.layer.backgroundColor = UIColor.red.cgColor
@@ -107,23 +91,23 @@ class NewUserVC: UIViewController, UITextFieldDelegate {
             tfAddress.layer.backgroundColor = UIColor.red.cgColor
 
             tfidUniversity.layer.backgroundColor = UIColor.red.cgColor
-            
+
         }else{
-            
+
             if (tfPassword.text?.isEqual(tfVPassword.text))! {
-                
+
                 self.createUser {
-                    
+
                     //CODIGO
-                    
+
                 }
-                
+
             }else{
-                
+
                 SCLAlertView().showError("Alerta", subTitle: "Las contraseñas deben coincidir")
-                
+
             }
-            
+
         }
     }
     
